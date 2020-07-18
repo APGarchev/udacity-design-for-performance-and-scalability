62fc380a12ad6d9f17fcc4f8f2a590ee22c2536e
Apostol Garchev <apostol.garchev@gmail.com>
2020-07-18 23:56:38 +0300

    Terraform script added

---
+++ README.md
@@ -0,0 +1,31 @@
+### How to deploy infrastructure defined in Terraform
+
+Steps are:
+
+- Install Terraform
+- Setup `main.tf`
+- Navigate to directory that contain the `mani.tf`
+- Run `terraform init`, that's because when you create a new configuration — or check out an existing configuration from version control — you need to initialize the directory with terraform init.
+- Setup `variables.tf` that contains definitions of all variables
+- Validate your configuration. If your configuration is valid, Terraform will return a success message, run `terraform validate`
+- Apply the script
+
+```
+terraform apply -var 'profile=udacity-training' -var 'region=us-east-1'
+```
+
+### How to destroy infrastructure deployed by Terraform
+
+```
+terraform destory
+```
+
+**Note** If there are some infrastructure defined manually, for example the script created VPN, but you manually setup a security group, then the `terraform destory` process won't be able to clean up the VPN. You must remove the security group first.
+
+### How to find the name of an AWS AMI
+
+Run the command by providing specifc region and AMI.
+
+```
+aws ec2 describe-images --region us-east-1 --image-ids ami-08f3d892de259504d
+```
---
+++ terraform-scripts/part-1/main.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.*.*.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name           = "Udacity VPC"
+    Project        = "Udacity"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id        = aws_vpc.main.id
+  cidr_block    = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "Udacity Public Subnet"
+    Project     = "Udacity"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id        = aws_vpc.main.id
+
+  tags = {
+    Name        = "Udacity IGW"
+    Project     = "Udacity"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "Udacity Route Table"
+    Project     = "Udacity"
+  }
+}
+
+resource "aws_main_route_table_association" "rta" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH Inbound Traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name        = "Udacity Allow SSH SG"
+    Project     = "Udacity"
+  }
+}
+
+resource "aws_instance" "t2_server" {
+  count         = 1
+  subnet_id     = aws_subnet.public.id
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+  key_name      = "${var.region}-key"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name        = "Udacity T2 (${count.index + 1})"
+    Project     = "Udacity"
+  }
+}
